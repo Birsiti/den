f8c7c54ca7f5c5eed49194294233eb57dc55f411
AlexeyShakal <tester_web@mail.ru>
2018-02-05 19:45:13 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,6 @@
+<? require_once ('templates/top.php'); ?>
+
+
+<h2> Добро пожаловать: </h2>
+
+<? require_once ('templates/top.php'); ?>
---
+++ templates/top.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html>
+<head>
+<meta charset='utf-8'>
+<title> проект  </title> 
+<meta name='discription' content="текст о проекте">
+<meta name='keywords' content="текст о проекте">
+<link rel='stylesheet' href='media/css/style.css' />
+<link rel='stylesheet' href='media/bootstrap-3.3.7/css/bootstrap.min.css' />
+<meta name='автор' >
+</head>
+<body>
+<div id='header'>
+<img src='media/img/logo.png' alt='логотип' id='logo'>
+<h1 class='logotext'>Name of site</h1>
+</div>
+<div>
+<nav class='menutop'>
+<a href="#" >главная</a>
+<a href="#" >новости</a>
+<a href="#" >разное</a>
+<a href="#" >иное</a>
+<a href="#" >прочее</a>
+    </nav>
+</div>
+<div class='col-md-2'>
+муню
+<a href='#' class='btn btn-block btn-success' >
+разработка сайтов
+</a>
+
+<a href='#' class='btn btn-block btn-primary' >
+продвижеен сайтов
+</a>
+<a href='#' class='btn btn-block btn-primary' >
+продвижеен сайтов
+</a>
+<a href='#' class='btn btn-block btn-primary' >
+продвижеен сайтов
+</a>
+</div>
+<div class='col-md-8'>
